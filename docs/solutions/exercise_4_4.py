a0cf4b68cc88f0ca9126820cf4aeef551002c81a
Willi Kappler <willi.kappler@uni-tuebingen.de>
2025-10-23 15:22:10 +0200

    Fix bug: endless loop.

--- docs/solutions/exercise_4_4.py
+++ docs/solutions/exercise_4_4.py
@@ -13,6 +13,6 @@ i = 0
 while i < len(temp):
     if temp[i] < 0.0:
         radar[i] = radar[i] * 10
-        i = i + 1
+    i = i + 1
 
 data.to_csv("temperature_amplitude_corrected.txt", header=["temperature", "radar amplitudes"], index=False)
