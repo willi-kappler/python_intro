2a82d70cf55c8153c4619383379cc5b839e069b6
Willi Kappler <willi.kappler@uni-tuebingen.de>
2025-10-23 10:09:29 +0200

    Use an actual function.

--- docs/solutions/exercise_3_2.py
+++ docs/solutions/exercise_3_2.py
@@ -1,16 +1,22 @@
-all_numbers = []
-total_sum = 0
+def get_numbers():
+    all_numbers = []
+    total_sum = 0
+    
+    while True:
+        print(f"Total sum: {total_sum}")
+        user_in = input("Enter a number: ")
+    
+        if user_in == "quit":
+            break
+    
+        number = int(user_in)
+        total_sum += number
+        all_numbers.append(number)
+        
+    return all_numbers
 
-while True:
-    print(f"Total sum: {total_sum}")
-    user_in = input("Enter a number: ")
+numbers = get_numbers()
 
-    if user_in == "quit":
-        break
-
-    number = int(user_in)
-    total_sum += number
-    all_numbers.append(number)
-
-all_numbers.sort()
-print(f"numbers: {all_numbers}")
+numbers.sort()
+print(f"numbers: {numbers}")
+1
