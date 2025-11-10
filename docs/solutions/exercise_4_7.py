04f5bb1d0b05db5fd646477c4dfb18b2b7708a95
Willi Kappler <willi.kappler@uni-tuebingen.de>
2025-11-10 11:44:00 +0100

    Add another solution for the non-recursive fibonacci function.

--- docs/solutions/exercise_4_7.py
+++ docs/solutions/exercise_4_7.py
@@ -1,13 +1,40 @@
-def fibonacci(n):
-     a, b = 0, 1
+def fibonacci1(n):
+    (a, b) = (0, 1)
+    # a = 0
+    # b = 1
 
-     for _ in range(n):
-         a, b, = b, a + b
+    for _ in range(n):
+        (a, b) = (b, a + b)
+        # tp = (b, a + b)
+        # a = tp[0]
+        # b = tp[1]
+        # a = b
+        # b = a + b
 
-     return a
+    return a
 
-print(f"F0: {fibonacci(0)}")
-print(f"F1: {fibonacci(1)}")
-print(f"F2: {fibonacci(2)}")
-print(f"F3: {fibonacci(3)}")
-print(f"F9: {fibonacci(9)}")
+
+def fibonacci2(n):
+    fib = [0, 1]
+
+    for i in range(2, n + 1):
+        new = fib[i - 1] + fib[i - 2]
+        fib.append(new)
+        print(fib)
+
+    return fib[n]
+
+
+print(f"F0: {fibonacci1(0)}")
+print(f"F1: {fibonacci1(1)}")
+print(f"F2: {fibonacci1(2)}")
+print(f"F3: {fibonacci1(3)}")
+print(f"F9: {fibonacci1(9)}")
+
+print("\n\n")
+
+print(f"F0: {fibonacci2(0)}")
+print(f"F1: {fibonacci2(1)}")
+print(f"F2: {fibonacci2(2)}")
+print(f"F3: {fibonacci2(3)}")
+print(f"F9: {fibonacci2(9)}")
