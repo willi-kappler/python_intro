b0c1bb344c7b1601a988947601ae7f5fdccbdf40
Willi Kappler <willi.kappler@uni-tuebingen.de>
2025-11-11 11:07:05 +0100

    Add more comments and a third alternative.

--- docs/solutions/exercise_4_7.py
+++ docs/solutions/exercise_4_7.py
@@ -5,11 +5,15 @@ def fibonacci1(n):
 
     for _ in range(n):
         (a, b) = (b, a + b)
+        # Alternative 1:
         # tp = (b, a + b)
         # a = tp[0]
         # b = tp[1]
-        # a = b
+
+        # Alternative 2:
+        # a2 = b
         # b = a + b
+        # a = a2
 
     return a
 
@@ -19,10 +23,24 @@ def fibonacci2(n):
 
     for i in range(2, n + 1):
         new = fib[i - 1] + fib[i - 2]
+        # The list will grow and consume memory
         fib.append(new)
         print(fib)
 
     return fib[n]
+
+
+def fibonacci3(n):
+    fib = [0, 1]
+
+    for i in range(2, n + 2):
+        new = fib[1] + fib[0]
+        fib.append(new)
+        # Here we always delete the first element to keep the list short:
+        del fib[0]
+        print(fib)
+
+    return fib[0]
 
 
 print(f"F0: {fibonacci1(0)}")
@@ -38,3 +56,11 @@ print(f"F1: {fibonacci2(1)}")
 print(f"F2: {fibonacci2(2)}")
 print(f"F3: {fibonacci2(3)}")
 print(f"F9: {fibonacci2(9)}")
+
+print("\n\n")
+
+print(f"F0: {fibonacci3(0)}")
+print(f"F1: {fibonacci3(1)}")
+print(f"F2: {fibonacci3(2)}")
+print(f"F3: {fibonacci3(3)}")
+print(f"F9: {fibonacci3(9)}")
